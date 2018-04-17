99ad25d60eb4da3e8daaeeb9389ef1a4d5436070
GuieMigi <florinchiriac2004@yahoo.com>
2018-04-17 20:57:29 +0300

    Added Parks Tab content

--- app/src/main/java/com/example/android/tourguide/ParksFragment.java
+++ app/src/main/java/com/example/android/tourguide/ParksFragment.java
@@ -1,12 +1,17 @@
 package com.example.android.tourguide;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +27,43 @@ public class ParksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.pointofinterest_list, container, false);
+
+        //Create an ArrayList containing the PointOfInterest.
+        final ArrayList<PointOfInterest> pois = new ArrayList<>();
+        pois.add(new PointOfInterest(getString(R.string.parcul_herastrau_name),
+                getString(R.string.parcul_herastrau_address), getString(R.string.parcul_herastrau_description),
+                R.drawable.parcul_herastrau));
+        pois.add(new PointOfInterest(getString(R.string.parcul_cismigiu_name),
+                getString(R.string.parcul_cismigiu_address), getString(R.string.parcul_cismigiu_description),
+                R.drawable.parcul_cismigiu));
+        pois.add(new PointOfInterest(getString(R.string.parcul_carol_name),
+                getString(R.string.parcul_carol_address), getString(R.string.parcul_carol_description),
+                R.drawable.parcul_carol));
+        pois.add(new PointOfInterest(getString(R.string.parcul_alexandru_ioan_cuza_name),
+                getString(R.string.parcul_alexandru_ioan_cuza_address), getString(R.string.parcul_alexandru_ioan_cuza_description),
+                R.drawable.parcul_alexandru_ioan_cuza));
+        pois.add(new PointOfInterest(getString(R.string.parcul_tineretului_name),
+                getString(R.string.parcul_tineretului_address), getString(R.string.parcul_tineretului_description),
+                R.drawable.parcul_tineretului));
+
+        final ListView listView = rootView.findViewById(R.id.list_view);
+        PointOfInterestAdapter poiAdapter = new PointOfInterestAdapter(getActivity(), pois);
+        listView.setAdapter(poiAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                PointOfInterest pointOfInterest = (PointOfInterest) listView.getItemAtPosition(position);
+                Intent openPoiDetails = new Intent(getContext(), PoiDetails.class);
+                openPoiDetails.putExtra("POI_NAME", pointOfInterest.getPoiName());
+                openPoiDetails.putExtra("POI_ADDRESS", pointOfInterest.getPoiAddress());
+                openPoiDetails.putExtra("POI_DESCRIPTION", pointOfInterest.getPoiDescription());
+                openPoiDetails.putExtra("POI_IMAGE", pointOfInterest.getPoiImage());
+                startActivity(openPoiDetails);
+            }
+        });
+        return rootView;
     }
 
 }
